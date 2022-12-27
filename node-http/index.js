b242d615095eee6b1bcf473f13d53c683d8fddab
Naman <jawaliyan@gmail.com>
2022-12-27 18:46:14 +0530

    Node HTTP Example 2

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,52 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
+    console.log("Request for " + req.url + " by method " + req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body>Hello World</body></html>');
+    if (req.method == 'GET') {
+        var fileUrl;
+
+        if (req.url == '/') {
+            fileUrl = 'index.html';
+        }
+        else {
+            fileUrl = req.url;
+        }
+
+        const filePath = path.resolve('./public/' + fileUrl);
+        const fileExt = path.extname(filePath);
+
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('Err 404: File not found');
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('Err 404: File not found');
+            return;
+        }
+    }
+    else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('Err 404: File not found');
+        return;
+    }
 });
 
 server.listen(port, hostname, () => {
