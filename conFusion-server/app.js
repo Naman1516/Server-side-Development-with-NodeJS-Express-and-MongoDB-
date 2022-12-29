ed24b0c455ca0a82c55744b8327e8e978d99e2cf
Naman <jawaliyan@gmail.com>
2022-12-29 15:59:46 +0530

    added basic authentication

--- conFusion-server/app.js
+++ conFusion-server/app.js
@@ -10,10 +10,32 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+    next(); // authorized
+  } else {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+}
+
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-
-const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/confusion';
 const connect = mongoose.connect(url);
@@ -33,6 +55,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// auth
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
